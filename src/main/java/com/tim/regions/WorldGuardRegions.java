dabffe8db52f1331f660e4be3cbc347d996dc861
gamesuchti <timbo0411@hotmail.com>
2024-06-19 22:09:49 +0200

    every region now gets the same flags

--- src/main/java/com/tim/regions/WorldGuardRegions.java
+++ src/main/java/com/tim/regions/WorldGuardRegions.java
@@ -1,11 +1,13 @@
 package com.tim.regions;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.world.World;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.Bukkit;
 
 import java.util.Collections;
@@ -18,11 +20,12 @@ public class WorldGuardRegions {
         if (regionManager == null) {
             System.out.println(Collections.emptyList());
         }
-        System.out.println(regionManager.getRegion("test").getPoints());
-        System.out.println(regionManager.getRegion("test").getMinimumPoint());
-        regionManager.getRegion("test").setFlag(Flags.BLOCK_BREAK, StateFlag.State.DENY);
-        regionManager.getRegion("test").setFlag(Flags.GREET_TITLE, "§2§l " + regionManager.getRegion("test").getId());
-        regionManager.getRegion("test").setFlag(Flags.FAREWELL_MESSAGE, "§3Goodbye!");
-        regionManager.getRegion("test").setFlag(Flags.DENY_MESSAGE, "§cDu darfst das nicht tun!");
+
+        for (ProtectedRegion region : regionManager.getRegions().values()) {
+            region.setFlag(Flags.BLOCK_BREAK, StateFlag.State.DENY);
+            region.setFlag(Flags.GREET_TITLE, "§2§l " + region.getId());
+            region.setFlag(Flags.FAREWELL_TITLE, "");
+            region.setFlag(Flags.DENY_MESSAGE, "§cDu darfst das nicht tun!");
+        }
     }
 }
